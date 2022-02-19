255db2093975ab0f7e7a3902d72155b94694e6b3
Aron Selhorst <contato@insource.com.br>
2022-02-19 17:43:57 -0300

    commit 'master'

---
+++ church-template/assets/config.php
@@ -0,0 +1,40 @@
+<?php
+
+    define( 'DB_HOST', 'localhost' ); // set database host 
+    define( 'DB_USER', 'root' );
+    define( 'DB_PASS', '' );
+    define( 'DB_NAME', 'mevamcha_mevam');
+    define( 'DISPLAY_DEBUG', false );
+ 
+
+    define( 'USERNAMEMAIL', 'contato@trass.com.br' );
+    define( 'USERNAMPASS', 'sflr2917A$' );
+    require_once( 'assets/class.db.php' );
+
+    $database   = new DB(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+    // $dbpreta     = new DB(DB_HOST_PRETA,DB_USER_PRETA,DB_PASS_PRETA,DB_NAME_PRETA);
+    date_default_timezone_set('America/Sao_Paulo');
+
+      
+    require "assets/class/urlfr.php";
+    function valida_campos($campo,$conn){return mysqli_real_escape_string($conn,htmlspecialchars($campo));}
+    function soNumero($str) {return preg_replace("/[^0-9]/", "", $str);}
+    function mes($m) {
+        switch ($m) {
+            case '1': $mm = "Jan"; break;
+            case '2': $mm = "Fev"; break;
+            case '3': $mm = "Mar"; break;
+            case '4': $mm = "Abr"; break;
+            case '5': $mm = "Mai"; break;
+            case '6': $mm = "Jun"; break;
+            case '7': $mm = "Jul"; break;
+            case '8': $mm = "Ago"; break;
+            case '9': $mm = "Set"; break;
+            case '10': $mm = "Out"; break;
+            case '11': $mm = "Nov"; break;
+            case '12': $mm = "Dez"; break;
+            default: $mm = "";break;
+        }
+        return $mm;
+    }
+?>
